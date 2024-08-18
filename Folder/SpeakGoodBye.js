d9830e95780405a2b05db833360b215473c98bd6
dindo-techs-it <161918087+just-tech-it@users.noreply.github.com>
2024-08-18 21:47:47 +0900

    Update SpeakGoodBye.js

--- Folder/SpeakGoodBye.js
+++ Folder/SpeakGoodBye.js
@@ -1,8 +1,9 @@
 (function(window) {
+	var byeSpeaker = {};
 	var speakWord = "Good Bye";
-	var byeSpeaker = function (name) {
-  		console.log(speakWord + " " + name);
-	}
+	 byeSpeaker.speak = function (name) {
+         console.log(speakWord + " " + name);
+  };
 	
 	window.byeSpeaker = byeSpeaker;
 
